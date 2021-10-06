f061489d5ce6bec7e1f603ec12ddaa3d277575a7
PedroRocs <pedrorochacardoso1908@gmail.com>
2021-10-05 22:24:21 -0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
 import TodoList from './components/Todo.List';
-import background from './components/background';
+
 
 
 function App() {
   return (
     <div className="todo-app">
-      <background />
+     
      <h1>LEMBRETES</h1>
      <TodoList />
     </div>
